47f3b66ef2471605924fa350192ec075aae84848
yzimroni <yanay03@gmail.com>
2017-08-25 22:25:15 +0300

    Inital commit!

---
+++ src/net/yzimroni/bukkitanimations/BukkitAnimationsPlugin.java
@@ -0,0 +1,17 @@
+package net.yzimroni.bukkitanimations;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BukkitAnimationsPlugin extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+
+}
