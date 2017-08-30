f436d35ea1ae3b5e7439cda9933fafc4c7ff98b3
yzimroni <yanay03@gmail.com>
2017-08-31 00:30:26 +0300

    Add alot of data handlers for entities & blocks

--- src/net/yzimroni/bukkitanimations/data/manager/MinecraftDataManagers.java
+++ src/net/yzimroni/bukkitanimations/data/manager/MinecraftDataManagers.java
@@ -10,27 +10,47 @@ import org.bukkit.DyeColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Rotation;
+import org.bukkit.block.Banner;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockFace;
 import org.bukkit.block.BlockState;
+import org.bukkit.block.CreatureSpawner;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Ageable;
+import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Creeper;
 import org.bukkit.entity.Enderman;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.FallingBlock;
 import org.bukkit.entity.Fireball;
+import org.bukkit.entity.Horse;
+import org.bukkit.entity.Horse.Color;
+import org.bukkit.entity.Horse.Style;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.ItemFrame;
 import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Minecart;
+import org.bukkit.entity.Ocelot;
+import org.bukkit.entity.Ocelot.Type;
 import org.bukkit.entity.Painting;
+import org.bukkit.entity.Pig;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
+import org.bukkit.entity.Rabbit;
+import org.bukkit.entity.Sheep;
+import org.bukkit.entity.Slime;
+import org.bukkit.entity.Tameable;
+import org.bukkit.entity.Villager;
+import org.bukkit.entity.Villager.Profession;
+import org.bukkit.entity.Wolf;
+import org.bukkit.entity.Zombie;
 import org.bukkit.material.Attachable;
 import org.bukkit.material.Colorable;
 import org.bukkit.material.FlowerPot;
 import org.bukkit.material.MaterialData;
 import org.bukkit.potion.PotionEffect;
+import org.bukkit.util.EulerAngle;
 import org.bukkit.util.Vector;
 
 import com.mojang.authlib.GameProfile;
@@ -313,8 +333,190 @@ public class MinecraftDataManagers {
 			}
 		});
 
+		ENTITIES.register(ArmorStand.class, new DataHandler<ArmorStand>() {
+
+			@Override
+			public void save(ActionData action, ArmorStand stand) {
+				action.data("headPose", stand.getHeadPose()).data("bodyPose", stand.getBodyPose())
+						.data("leftArmPose", stand.getLeftArmPose()).data("rightArmPose", stand.getRightArmPose())
+						.data("leftLegPose", stand.getLeftLegPose()).data("rightLeftPose", stand.getRightLegPose());
+
+				action.data("basePlate", stand.hasBasePlate()).data("visible", stand.isVisible())
+						.data("arms", stand.hasArms()).data("small", stand.isSmall()).data("marker", stand.isMarker());
+
+				// Item in hand and armor are saved in the LivingEntity data handler
+			}
+
+			@Override
+			public void load(ActionData action, ArmorStand stand) {
+				stand.setHeadPose(getEulerAngle(action, "headPose"));
+				stand.setBodyPose(getEulerAngle(action, "bodyPose"));
+				stand.setLeftArmPose(getEulerAngle(action, "leftArmPose"));
+				stand.setRightArmPose(getEulerAngle(action, "rightArmPose"));
+				stand.setLeftLegPose(getEulerAngle(action, "leftLegPose"));
+				stand.setRightLegPose(getEulerAngle(action, "rightLeftPose"));
+
+				stand.setBasePlate((boolean) action.getData("basePlate"));
+				stand.setVisible((boolean) action.getData("visible"));
+				stand.setArms((boolean) action.getData("arms"));
+				stand.setSmall((boolean) action.getData("small"));
+				stand.setMarker((boolean) action.getData("marker"));
+			}
+
+			private EulerAngle getEulerAngle(ActionData action, String name) {
+				@SuppressWarnings("unchecked")
+				Map<String, Object> map = (Map<String, Object>) action.getData(name);
+				return new EulerAngle((double) map.get("x"), (double) map.get("y"), (double) map.get("z"));
+			}
+		});
+
+		ENTITIES.register(Horse.class, new DataHandler<Horse>() {
+
+			@Override
+			public void save(ActionData action, Horse object) {
+				action.data("color", object.getColor()).data("style", object.getStyle());
+			}
+
+			@Override
+			public void load(ActionData action, Horse object) {
+				object.setColor(Color.valueOf((String) action.getData("color")));
+				object.setStyle(Style.valueOf((String) action.getData("style")));
+			}
+		});
+
+		ENTITIES.register(Minecart.class, new DataHandler<Minecart>() {
+
+			@Override
+			public void save(ActionData action, Minecart object) {
+				action.data("displayBlock", object.getDisplayBlock().toItemStack()).data("displayBlockOffset",
+						object.getDisplayBlockOffset());
+			}
+
+			@Override
+			public void load(ActionData action, Minecart object) {
+				object.setDisplayBlock(action.getItemStack("displayBlock").getData());
+				object.setDisplayBlockOffset(action.getInt("displayBlockOffset"));
+			}
+		});
+
+		ENTITIES.register(Ocelot.class, new DataHandler<Ocelot>() {
+
+			@Override
+			public void save(ActionData action, Ocelot object) {
+				action.data("catType", object.getCatType());
+			}
+
+			@Override
+			public void load(ActionData action, Ocelot object) {
+				object.setCatType(Type.valueOf((String) action.getData("catType")));
+			}
+		});
+
+		ENTITIES.register(Pig.class, new DataHandler<Pig>() {
+
+			@Override
+			public void save(ActionData action, Pig object) {
+				action.data("saddle", object.hasSaddle());
+			}
+
+			@Override
+			public void load(ActionData action, Pig object) {
+				object.setSaddle((boolean) action.getData("saddle"));
+			}
+		});
+
+		ENTITIES.register(Rabbit.class, new DataHandler<Rabbit>() {
+
+			@Override
+			public void save(ActionData action, Rabbit object) {
+				action.data("rabbitType", object.getRabbitType());
+			}
+
+			@Override
+			public void load(ActionData action, Rabbit object) {
+				object.setRabbitType(Rabbit.Type.valueOf((String) action.getData("rabbitType")));
+			}
+		});
+
+		ENTITIES.register(Sheep.class, new DataHandler<Sheep>() {
+
+			@Override
+			public void save(ActionData action, Sheep object) {
+				action.data("sheared", object.isSheared());
+			}
+
+			@Override
+			public void load(ActionData action, Sheep object) {
+				object.setSheared((boolean) action.getData("sheared"));
+			}
+		});
+
+		ENTITIES.register(Slime.class, new DataHandler<Slime>() {
+
+			@Override
+			public void save(ActionData action, Slime object) {
+				action.data("size", object.getSize());
+			}
+
+			@Override
+			public void load(ActionData action, Slime object) {
+				object.setSize(action.getInt("size"));
+			}
+		});
+
+		ENTITIES.register(Tameable.class, new DataHandler<Tameable>() {
+
+			@Override
+			public void save(ActionData action, Tameable object) {
+				action.data("tamed", object.isTamed());
+			}
+
+			@Override
+			public void load(ActionData action, Tameable object) {
+				object.setTamed((boolean) action.getData("tamed"));
+			}
+		});
+
+		ENTITIES.register(Villager.class, new DataHandler<Villager>() {
+
+			@Override
+			public void save(ActionData action, Villager object) {
+				action.data("profession", object.getProfession());
+			}
+
+			@Override
+			public void load(ActionData action, Villager object) {
+				object.setProfession(Profession.valueOf((String) action.getData("profession")));
+			}
+		});
+
+		ENTITIES.register(Zombie.class, new DataHandler<Zombie>() {
+
+			@Override
+			public void save(ActionData action, Zombie object) {
+				action.data("baby", object.isBaby());
+			}
+
+			@Override
+			public void load(ActionData action, Zombie object) {
+				object.setBaby((boolean) action.getData("baby"));
+			}
+		});
+
+		ENTITIES.register(Wolf.class, new DataHandler<Wolf>() {
+
+			@Override
+			public void save(ActionData action, Wolf object) {
+				action.data("collarColor", object.getCollarColor());
+			}
+
+			@Override
+			public void load(ActionData action, Wolf object) {
+				object.setCollarColor(DyeColor.valueOf((String) action.getData("collarColor")));
+			}
+		});
 		/*
-		 * TODO AreaEffectCloud ArmorStand
+		 * TODO AreaEffectCloud Firework
 		 */
 	}
 
@@ -374,16 +576,47 @@ public class MinecraftDataManagers {
 
 			@Override
 			public void save(ActionData action, FlowerPot object) {
-				action.data("contens", object.getContents());
+				action.data("contents", object.getContents());
 			}
 
 			@Override
 			public void load(ActionData action, FlowerPot object) {
-				if (action.has("contens")) {
-					object.setContents((MaterialData) action.getData("contens"));
+				if (action.has("contents")) {
+					object.setContents((MaterialData) action.getData("contents"));
 				}
 			}
 		});
+
+		BLOCKS.register(Banner.class, new DataHandler<Banner>() {
+			// TODO patterns
+
+			@Override
+			public void save(ActionData action, Banner object) {
+				action.data("baseColor", object.getBaseColor());
+			}
+
+			@Override
+			public void load(ActionData action, Banner object) {
+				object.setBaseColor(DyeColor.valueOf((String) action.getData("baseColor")));
+			}
+		});
+
+		BLOCKS.register(CreatureSpawner.class, new DataHandler<CreatureSpawner>() {
+
+			@Override
+			public void save(ActionData action, CreatureSpawner object) {
+				action.data("spawnedType", object.getSpawnedType());
+			}
+
+			@Override
+			public void load(ActionData action, CreatureSpawner object) {
+				object.setSpawnedType(EntityType.valueOf((String) action.getData("spawnedType")));
+			}
+		});
+
+		/*
+		 * TODO skull
+		 */
 	}
 
 	public static DataManager getEntities() {
